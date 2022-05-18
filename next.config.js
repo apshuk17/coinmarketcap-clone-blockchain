fe871945b26455e10d3b1b9b46849675a6124b1e
Apoorva Shukla <aposhukl@WKMZTB5622B1.global.publicisgroupe.net>
2022-05-18 06:03:12 +0530

    Add Header Component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "s2.coinmarketcap.com",
+      "s3.coinmarketcap.com",
+      "cdn.sanity.io",
+      "encrypted-tbn0.gstatic.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
